86fad729d9f352f2ae0031337f394b99fe594c80
Mark Jung <gujung2022@u.northwestern.edu>
2018-06-21 07:40:22 +0900

    cleanup before making check out page. Made the 404 more reasonable.

--- perf/src/pages/NoMatch/NoMatch.js
+++ perf/src/pages/NoMatch/NoMatch.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
-import '../../App.css';
+import './styles.css';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 
@@ -9,12 +9,8 @@ class NoMatchComponent extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">No such endpoint exists</h1>
+          <h1 className="App-title">404. No such endpoint exists.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
